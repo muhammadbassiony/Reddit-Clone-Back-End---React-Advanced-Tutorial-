101b5edcf4389887b1710c828fac308afb3372fb
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-11-07 03:05:55 +0200

    graphql crud test 1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
+import "reflect-metadata";
 
 import  express  from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { buildSchema } from 'type-graphql';
 
-import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import { Post } from './entities/Post';
 import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 
 
@@ -20,9 +22,10 @@ const main = async () => {
 
     const apolloServer = new ApolloServer({
         schema: await buildSchema({
-            resolvers: [ HelloResolver ],
+            resolvers: [ HelloResolver, PostResolver ],
             validate: false
-        })
+        }),
+        context: ({ em: orm.em })
     });
 
     await apolloServer.start();
