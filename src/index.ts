612937965db2b94dbe474f19cb18921e29fe7bb9
Muhammad Bassiony <31866582+muhammadbassiony@users.noreply.github.com>
2021-11-11 17:05:01 +0200

    logout

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
+import { COOKIE_NAME, __prod__ } from "./constants";
 import "reflect-metadata";
 
 import express from "express";
@@ -36,7 +36,7 @@ const main = async () => {
 
   app.use(
     session({
-      name: "qid",
+      name: COOKIE_NAME,
       store: new RedisStore({
         client: redisClient,
         disableTouch: true,
